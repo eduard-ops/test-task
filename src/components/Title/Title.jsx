e65af6626cf7f1b6eef32e33dd671073881a0ba4
eduard-ops <epustynnik@gmail.com>
2022-06-23 08:30:04 +0300

    Title\Title.jsx

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,3 +1,9 @@
+import PropTypes from 'prop-types';
+
 export default function Title({ title }) {
   return <h1 className="title-categories">{title}</h1>;
 }
+
+Title.propTypes = {
+  title: PropTypes.string.isRequired,
+};
